ac54ab59f420c416ab2617ace6dbff044e5636f5
Aidan Smith <aidansmith1243@gmail.com>
2019-11-21 14:35:34 -0600

    added the js for client side code

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -2,3 +2,52 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var movieEntrees = document.getElementsByClassName("movie-entry");
+
+var form = document.getElementById("search-and-filter-form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var term = document.getElementById("search").value;
+
+    var mpaa = [];
+    var mpaaCheckBox = document.getElementsByClassName("mpaa");
+    for (var j = 0; j < mpaaCheckBox.length; j++){
+        if (mpaaCheckBox[j].checked) {
+            mpaa.push(mpaaCheckBox[j].value);
+        }
+    }
+
+    var minIMDB = document.getElementById("imdb-min-rating");
+
+    for (var i = 0; i < movieEntrees.length; i++) {
+        var entry = movieEntrees[i];
+
+        // Unhide all Entrees
+        entry.style.display = "block";
+
+        // Filter by Search Term
+        if (term) {
+            if (!entry.dataset.title.toLowerCase().includes(term.toLowerCase())) {
+                entry.style.display = "none";
+            }
+        }
+
+        // Filter by mpaa rating
+        if (mpaa.length > 0) {
+            if (!mpaa.includes(entry.dataset.mpaa)) {
+                entry.style.display = "none";
+            }
+        }
+
+        // Filter by IMDB min rating
+        if (minIMDB) {
+            if (!entry.dataset.imdb || parseFloat(minIMDB) > parseFloat(entry.dataset.imdb)) {
+                entry.style.display = "none";
+            }
+        }
+
+    }
+
+});
